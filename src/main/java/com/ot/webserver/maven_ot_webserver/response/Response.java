c1a0f4fd6889e603f6acfa1063061f98870a82d4
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-01-24 02:07:37 +0100

    retrieve file/directory output from handler

--- src/main/java/com/ot/webserver/maven_ot_webserver/response/Response.java
+++ src/main/java/com/ot/webserver/maven_ot_webserver/response/Response.java
@@ -13,6 +13,7 @@ public class Response {
 	
 	private int statusCode;
 	private Socket socket;
+	private StringBuilder output;
 	private static final Logger logger = LogManager.getLogger(Response.class);
 	
 	public Response(int statusCode, Socket socket) {
@@ -20,23 +21,30 @@ public class Response {
 		this.socket = socket;
 	}
 	
+	public Response(int statusCode, Socket socket, StringBuilder output) {
+		this.statusCode = statusCode;
+		this.socket = socket;
+		this.output = output;
+	}
+	
 	public void responseView() {
 		try {
 			DataOutputStream outputStream = new DataOutputStream(this.socket.getOutputStream());
 			switch (this.statusCode) {
 				case 200:
-					
+					outputStream.writeBytes(this.output.toString());
+					outputStream.flush();
 					break;
 				case 401:
-					outputStream.writeBytes("<b> " + this.statusCode + " " + Config.STATUS_CODES.get(this.statusCode) + "</b>");
+					outputStream.writeBytes("<h1> " + this.statusCode + " " + Config.STATUS_CODES.get(this.statusCode) + "</h1>");
 					outputStream.flush();
 					break;			
 				case 404:
-					outputStream.writeBytes("<b> " + this.statusCode + " " + Config.STATUS_CODES.get(this.statusCode) + "</b>");
+					outputStream.writeBytes("<h1> " + this.statusCode + " " + Config.STATUS_CODES.get(this.statusCode) + "</h1>");
 					outputStream.flush();					
 					break;
 				case 405:
-					outputStream.writeBytes("<b> " + this.statusCode + " " + Config.STATUS_CODES.get(this.statusCode) + "</b>");
+					outputStream.writeBytes("<h1> " + this.statusCode + " " + Config.STATUS_CODES.get(this.statusCode) + "</h1>");
 					outputStream.flush();
 					break;
 				default:
