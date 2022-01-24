990e0fee453b7ef96a780a599eaa8ee8615239c7
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-01-24 02:05:14 +0100

    file & directory handling

--- src/main/java/com/ot/webserver/maven_ot_webserver/request/Handler.java
+++ src/main/java/com/ot/webserver/maven_ot_webserver/request/Handler.java
@@ -1,6 +1,7 @@
 package com.ot.webserver.maven_ot_webserver.request;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -30,8 +31,23 @@ public class Handler implements Runnable {
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 			String requestMethod = in.readLine().split("\\s+")[0];
+			// TODO check for header authorization
 			if(requestMethod.equalsIgnoreCase(Config.METHODS.GET.toString())) {
-				
+				String uri = in.readLine().split("\\s+")[1];
+				if(uri.equals("/")) {
+					
+				} else {
+					File file = new File(uri.substring(1));
+					if(file.exists()) {
+						
+					} else if (file.isDirectory()) {
+						
+					} else {
+						logger.info(uri + ": (404) " + Config.STATUS_CODES.get(404));
+						response = new Response(404, this.socket);
+						response.responseView();
+					}
+				}
 			} else {
 				logger.info(requestMethod.toUpperCase() + ": (405) " + Config.STATUS_CODES.get(405));
 				response = new Response(405, this.socket);
