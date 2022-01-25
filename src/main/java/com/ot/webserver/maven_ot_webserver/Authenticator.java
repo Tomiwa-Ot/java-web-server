4a2df53582991db827a6a399af6ae5f7031f2595
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-01-25 20:34:11 +0100

    Update Authenticator.java

--- src/main/java/com/ot/webserver/maven_ot_webserver/Authenticator.java
+++ src/main/java/com/ot/webserver/maven_ot_webserver/Authenticator.java
@@ -2,4 +2,18 @@ package com.ot.webserver.maven_ot_webserver;
 
 public class Authenticator {
 
+	private String username;
+	private String password;
+	
+	public Authenticator(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public boolean authenticate() {
+		if(this.username.equals(Config.username) && this.password.equals(Config.password)) {
+			return true;
+		}
+		return false;
+	}
 }
