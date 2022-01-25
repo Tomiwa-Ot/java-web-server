211daaa4c7038bb468dd27dc1992f7cdb3b2fdc8
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-01-25 22:32:47 +0100

    updates

--- src/main/java/com/ot/webserver/maven_ot_webserver/Authenticator.java
+++ src/main/java/com/ot/webserver/maven_ot_webserver/Authenticator.java
@@ -1,19 +1,41 @@
 package com.ot.webserver.maven_ot_webserver;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 public class Authenticator {
 
-	private String username;
-	private String password;
+	private static ArrayList<String> headers = new ArrayList<String>();
 	
-	public Authenticator(String username, String password) {
-		this.username = username;
-		this.password = password;
+	public static void authenticate(Socket s, String username, String password) {
+//		if(username.equals(Config.username) && password.equals(Config.password)) {
+//			
+//		}
+		// extract header authorzation basic base64
 	}
 	
-	public boolean authenticate() {
-		if(this.username.equals(Config.username) && this.password.equals(Config.password)) {
-			return true;
+	public static void responseView(Socket socket) throws IOException {
+		responseHeaders(200);
+		DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+		for(String header : headers) {
+			outputStream.writeBytes(header + "\r\n");
 		}
-		return false;
+		outputStream.writeBytes("\r\n");
+		outputStream.writeBytes("<!DOCTYPE html><html><head><title>Java Web Server</title></head><body></body></html>");
+		outputStream.writeBytes("\r\n");
+		outputStream.flush();
+	}
+	
+	public static void responseHeaders(int statusCode) {
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		headers.add("HTTP/1.0 " + Integer.toString(statusCode) + " " + Config.STATUS_CODES.get(statusCode));
+		headers.add("Content-Type: text/html");
+		headers.add("Date: " + formatter.format(new Date(System.currentTimeMillis())));
+		headers.add("Server: Simple Java Web Server");
+		headers.add("WWW-Authenticate: Basic");
 	}
 }
